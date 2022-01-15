62dddae89df188edb76ce46f1e371e25e8cf3b18
MaNu2026 <90133249+MaNu2026@users.noreply.github.com>
2022-01-16 01:43:27 +0530

    Forget Password

---
+++ ResetPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tudarmstadt.ukp.clarin.webanno.ui.core.resetPassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResetPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
